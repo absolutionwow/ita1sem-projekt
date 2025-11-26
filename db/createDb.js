7ceaeda17c661f53c0bb8ebc499bf0aa31b2969d
berthelsenmarius-debug <berthelsenmarius@gmail.com>
2025-11-26 10:41:10 +0100

    gjort så vi kan hente data

--- db/createDb.js
+++ db/createDb.js
@@ -11,20 +11,28 @@ console.log('All tables dropped.');
 
 console.log('Recreating tables...');
 await db.query(`
-    create table tracks (
-        track_id bigint primary key,
-	    title text not null,
-	    artist text not null,
-	    duration int not null
+CREATE TABLE tracks (
+    track_id BIGINT PRIMARY KEY,
+    title TEXT NOT NULL,
+    artist TEXT NOT NULL,
+    genre TEXT NOT NULL,
+    mood TEXT NOT NULL,
+    language TEXT NOT NULL,
+    bpm INT NOT NULL,
+    length_sec INT NOT NULL,
+    release_year INT NOT NULL,
+    vocal_instrumental TEXT NOT NULL,
+    mode TEXT NOT NULL
     )
 `);
 console.log('Tables recreated.');
 
 console.log('Importing data from CSV files...');
-await upload(db, 'db/short-tracks.csv', `
-	copy tracks (track_id, title, artist, duration)
+await upload(db, 'db/music.csv', `
+	copy tracks (track_id, title, artist, genre, mood, language, bpm, length_sec, release_year, vocal_instrumental, mode)
 	from stdin
-	with csv header`);
+	with csv header`
+);
 console.log('Data imported.');
 
 await db.end();
